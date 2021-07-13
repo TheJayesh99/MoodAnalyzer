f6d4e5313490e81f5c5b1850b03ed7bd64597c0d
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-13 18:36:14 +0530

    Added : Test case to return sad when user enters sad mood

--- lib/src/test/java/com/bridgelabz/moodanalyzertest/MoodAnalyseTest.java
+++ lib/src/test/java/com/bridgelabz/moodanalyzertest/MoodAnalyseTest.java
@@ -1,5 +1,19 @@
 package com.bridgelabz.moodanalyzertest;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.bridgelabz.moodanalyser.MoodAnalyserService;
+
 public class MoodAnalyseTest 
 {
+	MoodAnalyserService moodService = new MoodAnalyserService();
+	//check for sad mood
+	@Test
+	public void givenMessage_WhenSad_ShouldReturnSad() 
+	{
+		assertEquals("sad",moodService.analyseMood("i am in sad mood"));
+	}
+	
 }
